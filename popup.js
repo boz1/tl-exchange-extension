f21ad0e3cadedb6707fd45c6c3a821487dc0d719
boz <boz1@binghamton.edu>
2018-06-03 15:31:24 -0400

    1.3.0 publish

--- popup.js
+++ popup.js
@@ -1,22 +1,16 @@
 window.addEventListener('load', function load(event){
     let xhr1 = new XMLHttpRequest();
-    xhr1.open("GET", "https://api.fixer.io/latest?base=USD", false);
+    xhr1.open("GET", "http://www.apilayer.net/api/live?access_key=492cb37d1370bc6e68e128b0ecceeed0&format=1", false);
     xhr1.send();
     let myRespD = JSON.parse(xhr1.response)
-    let rateD = myRespD.rates.TRY
+    let rateD = myRespD.quotes.USDTRY.toFixed(4)
     document.getElementById("dollar-rate").innerHTML = rateD;
 
-    let xhr2 = new XMLHttpRequest();
-    xhr2.open("GET", "https://api.fixer.io/latest?base=EUR", false);
-    xhr2.send();
-    let myRespE = JSON.parse(xhr2.response)
-    let rateE = myRespE.rates.TRY
+    let rateDE = myRespD.quotes.USDEUR
+    let rateE = (rateD/rateDE).toFixed(4)
     document.getElementById("euro-rate").innerHTML = rateE;
 
-    let xhr3 = new XMLHttpRequest();
-    xhr3.open("GET", "https://api.fixer.io/latest?base=GBP", false);
-    xhr3.send();
-    let myRespP = JSON.parse(xhr3.response)
-    let rateP = myRespP.rates.TRY
+    let rateDP = myRespD.quotes.USDGBP
+    let rateP = (rateD/rateDP).toFixed(4)
     document.getElementById("pound-rate").innerHTML = rateP;
 })
