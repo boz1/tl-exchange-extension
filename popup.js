0173238fd00e311c370e71cef47c8e90e8ca5eb7
Burak Oz <boz1@binghamton.edu>
2018-08-30 11:55:08 -0700

    v1.8

--- popup.js
+++ popup.js
@@ -1,5 +1,26 @@
 window.addEventListener('load', function load(event) {
     document.getElementById("rateTable").style.display = "none"
+    document.getElementsByClassName("extra")[0].style.display = "none"
+    document.getElementsByClassName("extra")[1].style.display = "none"
+    document.getElementsByClassName("extra")[2].style.display = "none"
+    document.getElementsByClassName("arrow")[0].style.display = "none"
+    document.getElementsByClassName("arrow")[1].style.display = "none"
+
+    document.getElementById("down").addEventListener('click', function () {
+        document.getElementsByClassName("extra")[0].style.display = ""
+        document.getElementsByClassName("extra")[1].style.display = ""
+        document.getElementsByClassName("extra")[2].style.display = ""
+        document.getElementsByClassName("arrow")[0].style.display = "none"
+        document.getElementsByClassName("arrow")[1].style.display = ""
+    })
+
+    document.getElementById("up").addEventListener('click', function () {
+        document.getElementsByClassName("extra")[0].style.display = "none"
+        document.getElementsByClassName("extra")[1].style.display = "none"
+        document.getElementsByClassName("extra")[2].style.display = "none"
+        document.getElementsByClassName("arrow")[0].style.display = ""
+        document.getElementsByClassName("arrow")[1].style.display = "none"
+    })
 
     let urlUSD = 'https://cors-anywhere.herokuapp.com/' + 'https://www.investing.com/currencies/usd-try';
     let responseUSD = ""
@@ -8,8 +29,10 @@ window.addEventListener('load', function load(event) {
     xhr1.open("GET", urlUSD)
     xhr1.onload = function () {
         responseUSD = xhr1.responseText;
-        let curUsdRate = responseUSD.split('id="last_last" dir="ltr">')[1].split("</")[0]
-        document.getElementById("cur-usd-rate").appendChild(document.createTextNode(curUsdRate))
+        setCurrentRate(responseUSD, "cur-usd-rate")
+        setDiffRate(responseUSD, "diff-usd-rate")
+        setPerRate(responseUSD, "per-usd-rate")
+        setHistRate(responseUSD, "hist-usd-rate")
 
         let urlEUR = 'https://cors-anywhere.herokuapp.com/' + 'https://www.investing.com/currencies/eur-try';
         let responseEUR = ""
@@ -18,8 +41,10 @@ window.addEventListener('load', function load(event) {
         xhr2.open("GET", urlEUR)
         xhr2.onload = function () {
             responseEUR = xhr2.responseText;
-            let curEurRate = responseEUR.split('id="last_last" dir="ltr">')[1].split("</")[0]
-            document.getElementById("cur-eur-rate").appendChild(document.createTextNode(curEurRate))
+            setCurrentRate(responseEUR, "cur-eur-rate")
+            setDiffRate(responseEUR, "diff-eur-rate")
+            setPerRate(responseEUR, "per-eur-rate")
+            setHistRate(responseEUR, "hist-eur-rate")
 
             let urlGBP = 'https://cors-anywhere.herokuapp.com/' + 'https://www.investing.com/currencies/gbp-try';
             let responseGBP = ""
@@ -28,11 +53,16 @@ window.addEventListener('load', function load(event) {
             xhr3.open("GET", urlGBP)
             xhr3.onload = function () {
                 responseGBP = xhr3.responseText;
-                let curGbpRate = responseGBP.split('id="last_last" dir="ltr">')[1].split("</")[0]
-                document.getElementById("cur-gbp-rate").appendChild(document.createTextNode(curGbpRate))
+                setCurrentRate(responseGBP, "cur-gbp-rate")
+                setDiffRate(responseGBP, "diff-gbp-rate")
+                setPerRate(responseGBP, "per-gbp-rate")
+                setHistRate(responseGBP, "hist-gbp-rate")
 
-                document.getElementById('loader').style.display = "none"
-                document.getElementById('rateTable').style.display = "table"
+                document.getElementById("down").setAttribute("title","\u00d6nceki g\u00fcn ile kar\u015f\u0131la\u015ft\u0131r")
+                document.getElementsByClassName("hist")[0].setAttribute("title", "\u00d6nceki kapan\u0131\u015f")
+                document.getElementsByClassName("arrow")[0].style.display = ""
+                document.getElementById("loader").style.display = "none"
+                document.getElementById("rateTable").style.display = "table"
             };
             xhr3.send();
 
@@ -42,3 +72,33 @@ window.addEventListener('load', function load(event) {
     };
     xhr1.send();
 })
+
+function setCurrentRate(response, eid) {
+    let rate = response.split('id="last_last" dir="ltr">')[1].split("</")[0]
+    document.getElementById(eid).appendChild(document.createTextNode(rate))
+}
+
+function setDiffRate(response, eid) {
+    let rate = response.split('dir="ltr">')[2].split("</")[0]
+    if (rate.includes("+")) {
+        document.getElementById(eid).setAttribute("class", "up");
+    } else {
+        document.getElementById(eid).setAttribute("class", "down");
+    }
+    document.getElementById(eid).appendChild(document.createTextNode(rate))
+}
+
+function setPerRate(response, eid) {
+    let rate = response.split('dir="ltr">')[3].split("</")[0]
+    if (rate.includes("+")) {
+        document.getElementById(eid).setAttribute("class", "up");
+    } else {
+        document.getElementById(eid).setAttribute("class", "down");
+    }
+    document.getElementById(eid).appendChild(document.createTextNode(rate))
+}
+
+function setHistRate(response, eid) {
+    let rate = response.split('dir="ltr">')[4].split("</")[0]
+    document.getElementById(eid).appendChild(document.createTextNode(rate))
+}
